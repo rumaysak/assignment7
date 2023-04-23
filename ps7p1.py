4f0064901bacab131a8987cd0ccfa506afa16ab5
rumaysak <125397561+rumaysak@users.noreply.github.com>
2023-04-23 14:16:45 -0500

    Add files via upload

---
+++ ps7p1.py
@@ -0,0 +1,6 @@
+lname = input("Enter bowler's lat name")
+score = input("Enter 3 game scores")
+handicap = input("Enter handicap score")
+
+print("Total of average score of the bowler is  ", score)
+print("The average score with handicap is  ", handicap)
